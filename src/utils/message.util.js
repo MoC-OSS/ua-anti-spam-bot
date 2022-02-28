b6183fe73013a73edef99f8c74f489447e3c129a
Dmytro Vakulenko <dmytro.vakulenko@masterofcode.com>
2022-02-28 17:09:47 +0200

    feat: delete spaces in search text

--- src/utils/message.util.js
+++ src/utils/message.util.js
@@ -3,7 +3,7 @@ const rules = require('../../dataset/rules.json');
 
 class MessageUtil {
   findInText(message, searchFor) {
-    return message.replace(/ /g, '').toLowerCase().includes(searchFor.toLowerCase());
+    return message.replace(/ /g, '').toLowerCase().includes(searchFor.replace(/ /g, '').toLowerCase());
   }
 
   isHit(andCondition, rule, message) {
