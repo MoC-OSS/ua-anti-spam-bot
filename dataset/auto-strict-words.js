c1a99537f33e1d6e961f748ddf54fd3975760e63
Dmytro Vakulenko <dmytro.vakulenko@masterofcode.com>
2022-03-14 23:22:32 +0200

    feat(IMOCA-356): update auto strict words logic

--- dataset/auto-strict-words.js
+++ dataset/auto-strict-words.js
@@ -3,10 +3,11 @@ const fs = require('fs');
 const locations = require('./strings/locations.json');
 const strictLocations = require('./strings/strict_locations.json');
 
-const newFullLocations = locations.filter((location) => location.length > 5);
-const newShortLocations = [...locations.filter((location) => location.length <= 5), ...strictLocations];
+const symbolsCount = 5;
+
+const newFullLocations = locations.filter((location) => location.length > symbolsCount);
+const shortLocations = locations.filter((location) => location.length <= symbolsCount);
+const newStrictLocations = [...shortLocations, ...strictLocations];
 
 fs.writeFileSync('./strings/locations.json', `${JSON.stringify(newFullLocations, null, 2)}\n`);
-fs.writeFileSync('./strings/strict_locations.json', `${JSON.stringify(newShortLocations, null, 2)}\n`);
-
-require('./dataset-sort');
+fs.writeFileSync('./strings/strict_locations.json', `${JSON.stringify(newStrictLocations, null, 2)}\n`);
