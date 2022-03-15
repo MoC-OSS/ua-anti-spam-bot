e65b1854d895d359f0c91b856e208ba901031d79
Dmytro Vakulenko <dmytro.vakulenko@masterofcode.com>
2022-03-15 23:21:19 +0200

    feat(IMOCA-356): add optimized translit support

--- dataset/dataset.js
+++ dataset/dataset.js
@@ -1,18 +1,37 @@
 const fs = require('fs');
 const path = require('path');
 
+const CyrillicToTranslit = require('cyrillic-to-translit-js');
+
 const datasetPath = path.join(__dirname, './strings');
 const files = fs.readdirSync(datasetPath);
 const filePaths = files.map((filePath) => path.join(datasetPath, filePath));
 const fileKey = files.map((file) => file.split('.')[0]);
 
+const cyrillicToTranslit = new CyrillicToTranslit();
+
 const dataset = {};
 
+/**
+ * Import all datasets
+ * */
+console.info('*0 Initing dataset...');
 filePaths.forEach((filePath, index) => {
   const datasetName = fileKey[index];
   // eslint-disable-next-line global-require,import/no-dynamic-require
   dataset[datasetName] = require(filePath);
 });
+
+/**
+ * Add translit
+ * */
+console.info('*0 Add translit...');
+Object.keys(dataset).forEach((key) => {
+  const translitDataset = dataset[key].map((word) => cyrillicToTranslit.transform(word, ' '));
+  dataset[key] = [...dataset[key], ...translitDataset];
+});
+
+console.info('*0 Dataset is ready.');
 
 module.exports = {
   dataset,
