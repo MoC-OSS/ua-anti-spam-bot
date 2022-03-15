ee49005ee5196eb9d9b3612beedde87bc0215724
Dmytro Vakulenko <dmytro.vakulenko@masterofcode.com>
2022-03-15 22:33:32 +0200

    chore(IMOCA-356): fix append-excel-copy-in-locations.js

--- dataset/append-excel-copy-in-locations.js
+++ dataset/append-excel-copy-in-locations.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 
 const locations = require('./strings/locations.json');
+// eslint-disable-next-line import/no-unresolved
 const parsedLocations = require('./temp/parse.json');
 
 const newLocations = [...parsedLocations, ...locations];
