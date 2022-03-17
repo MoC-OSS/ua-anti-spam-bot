69301ee6dc046f7ed66bc149a295253374c1a473
Dmytro Vakulenko <dmytro.vakulenko@masterofcode.com>
2022-03-17 22:54:43 +0200

    chore(IMOCA-356): update auto-strict-words.js for high risk

--- dataset/auto-strict-words.js
+++ dataset/auto-strict-words.js
@@ -3,11 +3,28 @@ const fs = require('fs');
 const locations = require('./strings/locations.json');
 const strictLocations = require('./strings/strict_locations.json');
 
+const highRisk = require('./strings/high_risk.json');
+const strictHighRisk = require('./strings/strict_high_risk.json');
+
 const symbolsCount = 5;
 
-const newFullLocations = locations.filter((location) => location.length > symbolsCount);
-const shortLocations = locations.filter((location) => location.length <= symbolsCount);
-const newStrictLocations = [...shortLocations, ...strictLocations];
+function processDataset(fullDataset, strictDataset) {
+  const newFullDataset = fullDataset.filter((item) => item.length > symbolsCount);
+  const shortDataset = fullDataset.filter((item) => item.length <= symbolsCount);
 
-fs.writeFileSync('./strings/locations.json', `${JSON.stringify(newFullLocations, null, 2)}\n`);
-fs.writeFileSync('./strings/strict_locations.json', `${JSON.stringify(newStrictLocations, null, 2)}\n`);
+  const newStrictDataset = [...shortDataset, ...strictDataset];
+
+  return {
+    newFullDataset,
+    newStrictDataset,
+  };
+}
+
+const locationsResult = processDataset(locations, strictLocations);
+const highRiskResult = processDataset(highRisk, strictHighRisk);
+
+fs.writeFileSync('./strings/locations.json', `${JSON.stringify(locationsResult.newFullDataset, null, 2)}\n`);
+fs.writeFileSync('./strings/strict_locations.json', `${JSON.stringify(locationsResult.newStrictDataset, null, 2)}\n`);
+
+fs.writeFileSync('./strings/high_risk.json', `${JSON.stringify(highRiskResult.newFullDataset, null, 2)}\n`);
+fs.writeFileSync('./strings/strict_high_risk.json', `${JSON.stringify(highRiskResult.newStrictDataset, null, 2)}\n`);
