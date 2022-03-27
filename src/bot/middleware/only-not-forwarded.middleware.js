3144213493a18137ab80d8950be47cdfb5871e19
mzabolotnev <mzabolotnev@liveperson.com>
2022-03-27 12:26:36 +0200

    feat: delete wrong comment

--- src/bot/middleware/only-not-forwarded.middleware.js
+++ src/bot/middleware/only-not-forwarded.middleware.js
@@ -1,12 +1,6 @@
 /**
  * @description
  * Allow to skip a forwarded message
- *
- * Reversed copy from
- * @see https://github.com/backmeupplz/grammy-middlewares/blob/main/src/middlewares/onlyAdmin.ts
- *
- * @param {GrammyContext} ctx
- * @param {Next} next
  * */
 async function onlyNotForwarded(ctx, next) {
   // TODO use for ctx prod debug
