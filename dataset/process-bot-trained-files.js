0e7a56cf7fde19d290658cefcc3e9a37d624659e
Dmytro Vakulenko <dmytro.vakulenko@masterofcode.com>
2022-03-28 09:30:56 +0300

    fix(IMOCA-356): fix wrong imports

--- dataset/process-bot-trained-files.js
+++ dataset/process-bot-trained-files.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 
+// eslint-disable-next-line import/no-unresolved
 const positives = require('../positives.json');
+// eslint-disable-next-line import/no-unresolved
 const negatives = require('../negatives.json');
 
 fs.writeFileSync('../positives.csv', positives.join('\n'));
