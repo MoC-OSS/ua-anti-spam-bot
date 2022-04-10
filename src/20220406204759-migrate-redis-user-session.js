37938982ecdd3e79b01af56cfc597d524da2ee37
Dmytro Vakulenko <dmytro.vakulenko@masterofcode.com>
2022-04-10 15:06:46 +0300

    fix(IMOCA-356): fix migration logic

--- src/20220406204759-migrate-redis-user-session.js
+++ src/20220406204759-migrate-redis-user-session.js
@@ -14,67 +14,66 @@ const { redisService } = require('./services/redis.service');
 module.exports = async (bot, botStartDate) => {
   const compareDate = `${botStartDate.getFullYear()}-${botStartDate.getMonth() + 1}-${botStartDate.getDate()}`;
 
-  if (compareDate === '2022-4-10') {
+  if (compareDate !== '2022-4-10') {
+    console.info('Skip migration: ', __filename);
+    return;
+  }
+
+  /**
+   * @type {Session[]}
+   * */
+  const userRecords = await redisService.getUserSessions();
+
+  const getChatId = (sessionId) => sessionId.split(':')[0];
+  const uniqueUserRecords = userRecords.filter(
+    (session, index, self) => index === self.findIndex((t) => getChatId(t.id) === getChatId(session.id)),
+  );
+
+  // eslint-disable-next-line no-restricted-syntax
+  for (const record1 of uniqueUserRecords) {
+    const chatId = record1.id.split(':')[0];
+
     /**
-     * @type {Session[]}
+     * @type {ChatSessionData & SessionData}
      * */
-    const userRecords = await redisService.getUserSessions();
+    let chatSessionRecord = {};
 
-    const getChatId = (sessionId) => sessionId.split(':')[0];
-    const uniqueUserRecords = userRecords.filter(
-      (session, index, self) => index === self.findIndex((t) => getChatId(t.id) === getChatId(session.id)),
-    );
+    (async () => {
+      try {
+        const chat = await bot.api.getChat(chatId);
+        chatSessionRecord.chatType = chat.type;
+        chatSessionRecord.botRemoved = false;
 
-    // eslint-disable-next-line no-restricted-syntax
-    for (const record1 of uniqueUserRecords) {
-      const chatId = record1.id.split(':')[0];
+        if (chat.type !== 'private') {
+          chatSessionRecord.chatTitle = chat.title;
+          chatSessionRecord.chatMembersCount = await bot.api.getChatMemberCount(chatId);
 
-      /**
-       * @type {ChatSessionData}
-       * */
-      let chatSessionRecord = {};
-
-      bot.api
-        .getChat(chatId)
-        .then((chat) => {
-          chatSessionRecord.chatType = chat.type;
-          chatSessionRecord.botRemoved = false;
-
-          if (chat.type !== 'private') {
-            chatSessionRecord.chatTitle = chat.title;
-          }
-
-          return bot.api.getChatMemberCount(chatId);
-        })
-        .then((count) => {
-          chatSessionRecord.chatMembersCount = count;
-
-          return bot.api.getChatAdministrators(chatId);
-        })
-        .then((admins) => {
+          const admins = await bot.api.getChatAdministrators(chatId);
           const isBotAdmin = (admins || []).some((admin) => admin.user.id === bot.botInfo.id);
           chatSessionRecord.isBotAdmin = isBotAdmin;
 
           if (isBotAdmin) {
             chatSessionRecord.botAdminDate = new Date();
           }
-        })
-        .then(() => redisService.updateChatSession(chatId, chatSessionRecord))
-        .then(() => redisClient.removeKey(record1.id))
-        .then(() => console.info(`** Chat id has been migrated: ${record1.id}`))
-        .catch(() => {
-          chatSessionRecord = {
-            ...record1.data,
-          };
+        }
 
-          chatSessionRecord.botRemoved = true;
-          chatSessionRecord.isBotAdmin = false;
-          delete chatSessionRecord.botAdminDate;
+        await redisService.updateChatSession(chatId, chatSessionRecord);
+        await redisClient.removeKey(record1.id);
 
-          redisService.updateChatSession(chatId, chatSessionRecord).then(() => redisClient.removeKey(record1.id));
-        });
-    }
-  } else {
-    console.info('Skip migration: ', __filename);
+        console.info(`** Chat id has been migrated: ${record1.id}`);
+      } catch (e) {
+        console.info('Bot probably kicked: ', e);
+        chatSessionRecord = {
+          ...chatSessionRecord,
+          ...record1.data,
+        };
+
+        chatSessionRecord.botRemoved = true;
+        chatSessionRecord.isBotAdmin = false;
+        delete chatSessionRecord.botAdminDate;
+
+        redisService.updateChatSession(chatId, chatSessionRecord).then(() => redisClient.removeKey(record1.id));
+      }
+    })().then();
   }
 };
