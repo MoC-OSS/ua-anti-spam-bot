b75f0c862a79b14db77f170fd8fe2b4fbda7a250
Dmytro Vakulenko <dmytro.vakulenko@masterofcode.com>
2022-07-03 12:44:46 +0300

    feat(UABOT-2): add GrammyMiddleware type based on application ctx

--- src/types/context.ts
+++ src/types/context.ts
@@ -1,8 +1,10 @@
 import type { Context, SessionFlavor } from 'grammy';
 import type { ParseModeContext } from '@grammyjs/parse-mode';
+import { Middleware } from 'grammy/out/composer';
 
 import type { SessionData } from './session';
 import type { State, StateFlavor } from './state';
 import type { ChatSessionFlavor, ChatSessionData } from './session';
 
 export type GrammyContext = Context & ParseModeContext & SessionFlavor<SessionData> & ChatSessionFlavor<ChatSessionData> & StateFlavor<State>;
+export type GrammyMiddleware = Middleware<GrammyContext>;
