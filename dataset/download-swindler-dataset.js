41ef1ba681a1c67eeae65caba6aa67c3a3ce7242
Dmytro Vakulenko <dmytro.vakulenko@masterofcode.com>
2022-07-14 09:02:54 +0300

    chore(UABOT-25): update auto swindlers logic

--- dataset/download-swindler-dataset.js
+++ dataset/download-swindler-dataset.js
@@ -1,6 +1,7 @@
 const { swindlersGoogleService } = require('../src/services/swindlers-google.service');
 const { getSwindlersTopUsed } = require('./get-swindlers-top-used');
 const { autoSwindlers } = require('./auto-swindlers');
+const { initSwindlersContainer } = require('../src/services/swindlers.container');
 
 const cases = Promise.all([
   swindlersGoogleService.getTrainingPositives(),
@@ -14,6 +15,10 @@ console.info('Loading training messages...');
 cases.then(async ([positives, newSwindlersBots, testPositives, swindlersCards]) => {
   console.info('Received training messages.');
 
+  const { swindlersUrlsService, swindlersCardsService } = await initSwindlersContainer();
+
   getSwindlersTopUsed([...positives, ...testPositives]);
-  await autoSwindlers([...positives, ...testPositives], newSwindlersBots, swindlersCards);
+  await autoSwindlers(swindlersUrlsService, swindlersCardsService, [...positives, ...testPositives], newSwindlersBots, swindlersCards);
+
+  process.exit(0);
 });
