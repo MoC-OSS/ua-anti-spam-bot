67f2e0346a23ebc1ba5414236079e052984517dd
mzabolotnev <mzabolotnev@liveperson.com>
2022-07-21 17:40:46 +0200

    feat: create SSE connection

--- src/services/alarm.service.js
+++ src/services/alarm.service.js
@@ -1,15 +1,35 @@
 const { env } = require('typed-dotenv').config();
 const axios = require('axios');
+const events = require('events');
+
+const EventSource = require('eventsource');
 
 const apiUrl = 'https://alerts.com.ua/api/states';
 class AlarmService {
+  constructor() {
+    this.updatesEmitter = new events.EventEmitter();
+    this.subscribeOnNotifications();
+  }
+
   /**
    *
-   * @returns {Promise<any>}
+   * @returns {Promise< State[]>}
    * */
-  async getAlarms() {
+  async getStates() {
     const opts = { headers: { 'X-API-Key': env.ALARM_KEY } };
     return axios.get(apiUrl, opts).then((response) => response.data);
+  }
+
+  subscribeOnNotifications() {
+    const opts = { headers: { 'X-API-Key': env.ALARM_KEY } };
+    const source = new EventSource(apiUrl, opts);
+
+    source.onmessage = (event) => {
+      console.info('API EMIT DATA', event);
+      if (event.event === 'update') {
+        this.updatesEmitter.emit('alarm', event);
+      }
+    };
   }
 }
 
