93b5d04c7a3cc898080437cf3b4babc5fe314ebf
Dmytro Vakulenko <dmytro.vakulenko@masterofcode.com>
2022-09-28 22:34:18 +0300

    chore(UABOT-41): update ChatSessionData types

--- src/types/session.ts
+++ src/types/session.ts
@@ -1,4 +1,4 @@
-import { Chat } from 'typegram/manage';
+import type { Chat } from 'typegram/manage';
 
 export interface ChatSessionFlavor<S> {
   /**
@@ -49,16 +49,7 @@ export interface ChatSessionData {
     disableDeleteMessage: boolean;
     disableSwindlerMessage: boolean;
   }
-  chatPermissions?: {
-    can_send_messages: boolean;
-    can_send_media_messages: boolean;
-    can_send_polls: boolean;
-    can_send_other_messages: boolean;
-    can_add_web_page_previews: boolean;
-    can_change_info: boolean;
-    can_invite_users: boolean;
-    can_pin_messages: boolean;
-  }
+  chatPermissions?: Chat.MultiUserGetChat['permissions'];
 }
 
 export interface Session {
