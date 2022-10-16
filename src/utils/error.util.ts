737fad96b75645f5271c16b4f13af1064dd45fc4
Dmytro Vakulenko <dmytro.vakulenko@masterofcode.com>
2022-10-16 14:21:53 +0300

    feat(UABOT-41): add globalErrorHandler

--- src/utils/error.util.ts
+++ src/utils/error.util.ts
@@ -1,5 +1,21 @@
-import type { GrammyErrorHandler } from '../types';
+import type { ErrorHandler } from 'grammy';
+import { GrammyError, HttpError } from 'grammy';
+
+import type { GrammyContext } from '../types';
 
 export const handleError = (catchError: unknown, reason = '') => {
   console.error('**** HANDLED ERROR ****', reason, catchError);
+};
+
+export const globalErrorHandler: ErrorHandler<GrammyContext> = (botError) => {
+  const { ctx, error } = botError;
+  console.error(`Error while handling update ${ctx.update.update_id}:`);
+
+  if (error instanceof GrammyError) {
+    console.error('**** HANDLED ERROR **** Error in request:', error.description);
+  } else if (error instanceof HttpError) {
+    console.error('**** HANDLED ERROR **** Could not contact Telegram:', error);
+  } else {
+    console.error('**** HANDLED ERROR **** Unknown error:', error);
+  }
 };
