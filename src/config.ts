c9d01ddc8144ac0f98512a7c339f2c84ee82b6fa
Dmytro Vakulenko <dmytro.vakulenko@masterofcode.com>
2022-10-25 09:00:50 +0300

    feat(UABOT-48): move swindlers logic on server

--- src/config.ts
+++ src/config.ts
@@ -5,8 +5,8 @@ import type { EnvironmentConfig } from './types';
 const { error, env } = typedDotenv.config();
 
 if (error) {
-  // eslint-disable-next-line no-console
   console.error('Something wrong with env variables');
+  console.error(error);
   // eslint-disable-next-line unicorn/no-process-exit
   process.exit();
 }
