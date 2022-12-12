0a7ebe81ae5c1e9aabb65a307773d9143ccd7ab8
Dmytro Vakulenko <dmytro.vakulenko@masterofcode.com>
2022-12-12 10:15:00 +0200

    fix(UABOT-91): fix join leave if bot is not an admin

--- src/bot/filters/index.ts
+++ src/bot/filters/index.ts
@@ -4,4 +4,5 @@ export * from './only-active-default-setting.filter';
 export * from './only-active-optional-setting.filter';
 export * from './only-creator.filter';
 export * from './only-not-deleted.filter';
+export * from './only-when-bot-admin.filter';
 export * from './only-whitelisted.filter';
