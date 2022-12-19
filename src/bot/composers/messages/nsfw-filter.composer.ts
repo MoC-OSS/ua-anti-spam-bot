67dc8dbacaab096efed58242edf52e299e9f5772
Dmytro Vakulenko <dmytro.vakulenko@masterofcode.com>
2022-12-19 20:34:37 +0200

    feat(UABOT-98): update message for nsfw blocking

--- src/bot/composers/messages/nsfw-filter.composer.ts
+++ src/bot/composers/messages/nsfw-filter.composer.ts
@@ -1,7 +1,9 @@
 import { Composer } from 'grammy';
 
+import { getDeleteNsfwMessage } from '../../../message';
 import type { NsfwTensorService } from '../../../tensor';
 import type { GrammyContext } from '../../../types';
+import { getUserData } from '../../../utils';
 
 export interface NsfwFilterComposerProperties {
   nsfwTensorService: NsfwTensorService;
@@ -25,15 +27,10 @@ export const getNsfwFilterComposer = ({ nsfwTensorService }: NsfwFilterComposerP
         await context.deleteMessage();
 
         if (context.chatSession.chatSettings.disableDeleteMessage !== true) {
-          await context.reply(
-            `✅ Found ${predictionResult.deletePrediction.className} with ${predictionResult.deletePrediction.probability} probability.`,
-          );
+          const { writeUsername, userId } = getUserData(context);
+
+          await context.replyWithSelfDestructedHTML(getDeleteNsfwMessage({ writeUsername, userId }));
         }
-      } else {
-        await context.reply(
-          `⛔️ Not found. Highest ${predictionResult.highestPrediction.className} with ${predictionResult.highestPrediction.probability} probability.`,
-          { reply_to_message_id: context.msg?.message_id },
-        );
       }
     }
 
