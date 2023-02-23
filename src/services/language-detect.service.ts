0a9ea5bc344af51831083cfde44110f24965b2f1
Dmytro Vakulenko <dmytro.vakulenko@masterofcode.com>
2023-02-23 09:29:04 +0200

    feat(UABOT-124): fix language detection

--- src/services/language-detect.service.ts
+++ src/services/language-detect.service.ts
@@ -3,6 +3,10 @@ import * as cld from 'cld';
 import type { LanguageDetectionResult } from '../types';
 
 export class LanguageDetectService {
+  readonly russianLetters = /[ъыэё]/i;
+
+  readonly ukrainianLetters = /['єіїґ‘]/i;
+
   /**
    * Helper function that detects if the message in russian
    * */
@@ -10,12 +14,32 @@ export class LanguageDetectService {
     try {
       const detectResult = await this.detect(message);
 
+      /**
+       * If the message contains russian letters, we assume that this is russian
+       * */
+      if (this.russianLetters.test(message)) {
+        return {
+          result: false,
+          percent: 200,
+        };
+      }
+
       const russianDetect = detectResult.languages.find((language) => language.code === 'ru');
 
       if (!russianDetect) {
         return {
           result: false,
           percent: 0,
+        };
+      }
+
+      /**
+       * If the message contains ukrainian letters, we assume that this is ukrainian
+       * */
+      if (this.ukrainianLetters.test(message)) {
+        return {
+          result: false,
+          percent: 1,
         };
       }
 
