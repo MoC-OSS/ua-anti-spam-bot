19f689f6b888b008510cc29d7097e814b4ba2eb0
Dmytro Vakulenko <dmytro.vakulenko@masterofcode.com>
2023-02-23 10:02:55 +0200

    feat(UABOT-124): fix language detection by adding additional regex

--- src/services/language-detect.service.ts
+++ src/services/language-detect.service.ts
@@ -1,4 +1,5 @@
 import * as cld from 'cld';
+import { removeExtraSpaces, removeSpecialSymbols } from 'ukrainian-ml-optimizer';
 
 import type { LanguageDetectionResult } from '../types';
 
@@ -11,18 +12,18 @@ export class LanguageDetectService {
    * Helper function that detects if the message in russian
    * */
   async isRussian(message: string): Promise<LanguageDetectionResult> {
+    /**
+     * If the message contains russian letters, we assume that this is russian
+     * */
+    if (this.russianLetters.test(message)) {
+      return {
+        result: true,
+        percent: 200,
+      };
+    }
+
     try {
       const detectResult = await this.detect(message);
-
-      /**
-       * If the message contains russian letters, we assume that this is russian
-       * */
-      if (this.russianLetters.test(message)) {
-        return {
-          result: false,
-          percent: 200,
-        };
-      }
 
       const russianDetect = detectResult.languages.find((language) => language.code === 'ru');
 
@@ -63,7 +64,13 @@ export class LanguageDetectService {
    * @param message - message to get language
    * */
   detect(message: string) {
-    return cld.detect(message);
+    const clearMessage = removeExtraSpaces(removeSpecialSymbols(message)).toLowerCase();
+
+    return cld.detect(removeSpecialSymbols(clearMessage), {
+      isHTML: false,
+      languageHint: 'RUSSIAN',
+      tldHint: 'ru',
+    });
   }
 }
 
