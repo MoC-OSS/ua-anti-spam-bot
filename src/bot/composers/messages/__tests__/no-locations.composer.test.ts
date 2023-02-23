112289a9fd16caebaf7e615581dc052720816598
Dmytro Vakulenko <dmytro.vakulenko@masterofcode.com>
2023-02-23 23:27:53 +0200

    test(UABOT-128): fix no locations unit test

--- src/bot/composers/messages/__tests__/no-locations.composer.test.ts
+++ src/bot/composers/messages/__tests__/no-locations.composer.test.ts
@@ -33,7 +33,11 @@ describe('noLocationsComposer', () => {
 
     bot.use(noLocationsComposer);
 
-    outgoingRequests = await prepareBotForTesting<GrammyContext>(bot);
+    outgoingRequests = await prepareBotForTesting<GrammyContext>(bot, {
+      getChat: {
+        invite_link: '',
+      },
+    });
   }, 5000);
 
   describe('enabled feature', () => {
@@ -49,10 +53,17 @@ describe('noLocationsComposer', () => {
       const update = new MessagePrivateMockUpdate('Тут ТеРемкИ без сВітла').build();
       await bot.handleUpdate(update);
 
-      const [deleteMessageRequest, sendMessageRequest] = outgoingRequests.getTwoLast<'deleteMessage', 'sendMessage'>();
+      const [deleteMessageRequest, getChatRequest, sendLogsMessageRequest, sendMessageRequest] = outgoingRequests.getAll<
+        'deleteMessage',
+        'getChat',
+        'sendMessage',
+        'sendMessage'
+      >();
 
-      expect(outgoingRequests.length).toEqual(2);
+      expect(outgoingRequests.length).toEqual(4);
       expect(deleteMessageRequest?.method).toEqual('deleteMessage');
+      expect(getChatRequest?.method).toEqual('getChat');
+      expect(sendLogsMessageRequest?.method).toEqual('sendMessage');
       expect(sendMessageRequest?.method).toEqual('sendMessage');
     });
 
@@ -61,10 +72,16 @@ describe('noLocationsComposer', () => {
       const update = new MessagePrivateMockUpdate('Тут ТеРемкИ без сВітла').build();
       await bot.handleUpdate(update);
 
-      const deleteMessageRequest = outgoingRequests.getLast<'deleteMessage'>();
+      const [deleteMessageRequest, getChatRequest, sendLogsMessageRequest] = outgoingRequests.getAll<
+        'deleteMessage',
+        'getChat',
+        'sendMessage'
+      >();
 
-      expect(outgoingRequests.length).toEqual(1);
+      expect(outgoingRequests.length).toEqual(3);
       expect(deleteMessageRequest?.method).toEqual('deleteMessage');
+      expect(getChatRequest?.method).toEqual('getChat');
+      expect(sendLogsMessageRequest?.method).toEqual('sendMessage');
     });
 
     it('should not delete message without a location', async () => {
