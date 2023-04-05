bf6e0dfe1b8ea95a2537a61f3cf12b2ffe568138
Dmytro Vakulenko <dmytro.vakulenko@masterofcode.com>
2023-04-05 09:21:12 +0300

    feat(UABOT-137): add webview validate token POC endpoint

--- src/bot-express.server.ts
+++ src/bot-express.server.ts
@@ -1,9 +1,23 @@
+import cors from 'cors';
 import express from 'express';
 
 import { environmentConfig } from './config';
+import { verifyTelegramWebAppData } from './express-logic';
 
 export const runBotExpressServer = () => {
   const app = express();
+
+  app.use(express.json());
+
+  // TODO setup origin
+  app.use(cors({ origin: 'https://*.ngrok.io' }));
+
+  app.post('/validate', (request, response) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument,@typescript-eslint/no-unsafe-member-access
+    const isValid = verifyTelegramWebAppData(request.body.query);
+
+    response.json({ isValid });
+  });
 
   app.get('/health-check', (request, response) => response.json({ status: 'ok' }));
 
