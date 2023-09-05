4bf6a96585df8f0edd487732846d420c720b8630
Dmytro Vakulenko <dmytro.vakulenko@masterofcode.com>
2023-09-05 22:48:36 +0300

    feat(UABOT-118): exclude russian warship

--- src/services/obscene.service.ts
+++ src/services/obscene.service.ts
@@ -1,7 +1,25 @@
 import { dataset } from '../../dataset/dataset';
+import { SearchSet } from '../utils';
 
 export class ObsceneService {
+  private readonly warshipAllowList = new SearchSet(['корабль', 'корабель', 'кораблю']);
+
+  private readonly militaryAllowList = new SearchSet([
+    'военний',
+    'воєнний',
+    'воений',
+    'военный',
+    'военый',
+    'військовий',
+    'русский',
+    'російський',
+  ]);
+
   checkObscene(message: string) {
+    if (this.warshipAllowList.search(message) && this.militaryAllowList.search(message)) {
+      return null;
+    }
+
     return dataset.obscene_dictionary.search(message);
   }
 }
