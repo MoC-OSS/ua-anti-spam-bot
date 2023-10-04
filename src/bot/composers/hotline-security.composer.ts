0b805bf6e46610a1bf0a3f63f0915f9558de74a6
Dmytro Vakulenko <dmytro.vakulenko@masterofcode.com>
2023-10-04 12:51:48 +0300

    feat(UABOT-191): remove hotline_security command call

--- src/bot/composers/hotline-security.composer.ts
+++ src/bot/composers/hotline-security.composer.ts
@@ -2,6 +2,7 @@ import { Composer } from 'grammy';
 
 import { swindlersHelpMessage } from '../../message';
 import type { GrammyContext } from '../../types';
+import { handleError } from '../../utils';
 import { ignoreOld } from '../middleware';
 
 const mainPhotoFileId = 'AgACAgIAAxkBAAI3PGUdLT60yhtl3GLyAgXYSZ1IjO2NAALSzTEbKALoSCJ7nmsSxICQAQADAgADeAADMAQ';
@@ -13,6 +14,7 @@ export const getHotlineSecurityComposer = () => {
   const hotlineSecurityComposer = new Composer<GrammyContext>();
 
   hotlineSecurityComposer.command('hotline_security', ignoreOld(30), async (context) => {
+    await context.deleteMessage().catch(handleError);
     await context.replyWithPhotoWithSelfDestructedHTML(mainPhotoFileId, { caption: swindlersHelpMessage });
   });
 
