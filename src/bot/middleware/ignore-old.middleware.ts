e9087d7069d2bf3cf7dfa031fc98b7d78cad8420
Roman Dashko <roman.dashko@masterofcode.com>
2023-11-21 18:16:37 +0200

    feat(UABOT-117): fix old message detection

--- src/bot/middleware/ignore-old.middleware.ts
+++ src/bot/middleware/ignore-old.middleware.ts
@@ -3,11 +3,12 @@ import type { GrammyMiddleware } from '../../types';
 export const ignoreOld =
   (threshold = 5 * 60): GrammyMiddleware =>
   (context, next) => {
-    if (context.msg?.date && Date.now() / 1000 - context.msg.date > threshold) {
+    const date = context.editedMessage?.edit_date || context.msg?.date;
+    if (date && Date.now() / 1000 - date > threshold) {
       console.info(
-        `Ignoring message from user ${context.from?.id || '$unknown'} at chat ${context.chat?.id || '$unknown'} (${Date.now() / 1000}:${
-          context.msg.date
-        })`,
+        `Ignoring message from user ${context.from?.id || '$unknown'} at chat ${context.chat?.id || '$unknown'} (${
+          Date.now() / 1000
+        }:${date})`,
       );
       return;
     }
