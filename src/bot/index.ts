87633561b3d4fdb70d1f2330a16782993e187b0c
Dmytro Vakulenko <33941060+DrSmile444@users.noreply.github.com>
2026-03-18 20:18:42 +0200

    feat(UABOT-240): log memory usage information during bot execution

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -105,9 +105,9 @@ import { logsChat } from './creator';
   // Enable graceful stop
   const stopRunner = () => runner.isRunning() && runner.stop();
 
-  // const newMemoryUsage = process.memoryUsage();
-  //
-  // console.log(`Memory Usage: ${newMemoryUsage.rss / 1024 / 1024} MB`);
+  const newMemoryUsage = process.memoryUsage();
+
+  logger.info(`Memory Usage: ${newMemoryUsage.rss / 1024 / 1024} MB`);
 
   process.once('SIGINT', stopRunner);
 
